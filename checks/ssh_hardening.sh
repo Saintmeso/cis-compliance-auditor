b7058afad9431392b3a8cad78c5cd6a20f9c73aa
Saintmeso <ryalobaidi@icloud.com>
2026-06-01 15:36:09 +0300

    Added Login Grace Time check

--- checks/ssh_hardening.sh
+++ checks/ssh_hardening.sh
@@ -21,3 +21,15 @@ elif [ "$max_tries" -le 5 ]; then
 else
 	echo "[FAIL] Too many login attempts may be allowed."
 fi
+
+echo ""
+echo "[CHECK] Login Grace Time"
+
+grace_time=$(grep "^[[:space:]]*LoginGraceTime " /etc/ssh/sshd_config | awk '{print $2}')
+if [ -z "$grace_time" ]; then
+	echo "[FAIL] Login grace time is not configured"
+elif [ "$grace_time"  -le 60 ]; then
+	echo "[PASS] LoginGraceTime configured securely."
+else
+	echo "[FAIL] LoginGraceTime exceeds recommended threshold"
+fi
